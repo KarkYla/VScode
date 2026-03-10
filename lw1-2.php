957f02b9b0d55de146979a84e3336c2dc713f168
KarkYla <he_he_08@mail.ru>
2026-03-10 15:33:25 +0300

    lw1 variant-2 zadanie-2

---
+++ lw1-2.php
@@ -0,0 +1,20 @@
+<?php
+$sum = 500;
+$skidka = 0;
+$finalSUM = $sum;
+if ($sum >= 10000) {
+    $skidka = 20;  
+} elseif ($sum >= 5000 && $sum < 10000) {
+    $skidka = 10;
+} else {
+    $skidka = 0;
+}
+
+if ($skidka > 0) {
+    $sumSkidki = $sum * ($skidka / 100);
+    $finalSUM = $sum - $sumSkidki;
+}
+echo "Сумма покупки: $sum\n";
+echo "Скидка: $skidka%\n";
+echo "Итоговая сумма: $finalSUM\n";
+?>
