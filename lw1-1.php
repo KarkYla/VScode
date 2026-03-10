e6a78a494a48b5f6a850ac2763beea72f27685ee
KarkYla <he_he_08@mail.ru>
2026-03-10 15:57:18 +0300

    lw1 variant-2 zadanie-1

---
+++ lw1-1.php
@@ -0,0 +1,10 @@
+<?php
+$number = 19091;
+$str = (string)$number;
+$reversed = strrev($str);
+if ($str === $reversed) {
+    echo "число $number читается одинаково\n";
+} else {
+    echo "число $number не читается одинаково\n";
+}
+?>
