10c8b4f6dc6dbecbe0eab7db1c1fa33fcaa43170
KarkYla <he_he_08@mail.ru>
2026-03-13 16:36:09 +0300

    lw2-3 variant-2 zadanie-3

---
+++ lw2-3.php
@@ -0,0 +1,24 @@
+<?php
+
+function zigzag($arr) {
+    $n = count($arr);
+
+    if ($n <= 2) {
+        return true;
+    }
+    $expected = 0;
+    
+    for ($i = 1; $i < $n; $i++) {
+        $prev = $arr[$i - 1];
+        $curr = $arr[$i];
+
+        if ($curr > $prev) {
+            $direction = 1;
+        } elseif ($curr < $prev) {
+            $direction = -1;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
